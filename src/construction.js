1b3d6537da2d6cb67bd77a8422cd16bb932eca2e
Zac <zdobbs3@gmail.com>
2019-05-28 13:51:43 -0700

    adjusting smaller device styles for construction page

--- src/construction.js
+++ src/construction.js
@@ -9,16 +9,16 @@ class Construction extends Component {
     return (
       <div className="container-fluid banner d-flex">
         <div className="row">
-          <div className="brand">
+          <div className="brand col-10">
             <h1>Ardent/Works</h1>
             <p className="font-weight-light">Photography</p>
           </div>
-          <div className="message text-center">
+          <div className="message text-center col-10">
             <h2>New Website</h2>
             <h3>Under Construction</h3>
             <p>Thank you for your patience</p>
           </div>
-          <div className="ribbon text-center">
+          <div className="ribbon text-center col-10">
             <h3>Contact the Studio</h3>
             <span className="contactMe">602.793.9903</span>
           </div>
