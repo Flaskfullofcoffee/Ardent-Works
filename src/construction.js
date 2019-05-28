a0abe0b0951e9356b7aa8254907ead62deebf8ae
Zac <zdobbs3@gmail.com>
2019-05-28 12:59:55 -0700

    set up construction page styles

---
+++ src/construction.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Construction extends Component {
+
+
+  render() {
+    return (
+      <div className="container-fluid banner d-flex">
+        <div className="row">
+          <div className="brand">
+            <h1>Ardent/Works</h1>
+            <p className="font-weight-light">Photography</p>
+          </div>
+          <div className="message text-center">
+            <h2>New Website</h2>
+            <h3>Under Construction</h3>
+            <p>Thank you for your patience</p>
+          </div>
+          <div className="ribbon text-center">
+            <h3>Contact the Studio</h3>
+            <span className="contactMe">602.793.9903</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Construction;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Construction from './construction';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Construction />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
